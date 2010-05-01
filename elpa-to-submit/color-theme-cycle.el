20cc4e5924ad89b266090f07afd19987e704088d
Andreas Pauley <apauley+github@gmail.com>
2010-05-02 00:51:07 +0200

    Color theme cycler

---
+++ elpa-to-submit/color-theme-cycle.el
@@ -0,0 +1,30 @@
+;; Taken (with some slight modifications) from
+;; http://www.emacswiki.org/emacs/ColorTheme#toc7
+;; http://www.emacswiki.org/emacs/JakubHozak
+
+(require 'color-theme)
+(color-theme-initialize)
+
+(setq my-color-themes (mapcar #'car color-themes))
+(defun my-theme-set-default () ; Set the first row
+  (interactive)
+  (setq theme-current my-color-themes)
+  (funcall (car theme-current)))
+
+(defun my-describe-theme () ; Show the current theme
+  (interactive)
+  (message "%s" (car theme-current)))
+
+; Set the next theme (fixed by Chris Webber - tanks)
+(defun my-theme-cycle ()
+  (interactive)
+  (setq theme-current (cdr theme-current))
+  (if (null theme-current)
+      (setq theme-current my-color-themes))
+  (funcall (car theme-current))
+  (message "%S" (car theme-current)))
+
+(setq theme-current my-color-themes)
+(setq color-theme-is-global nil) ; Initialization
+(my-theme-set-default)
+(global-set-key [f6] 'my-theme-cycle)
